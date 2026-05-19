97d1863e35a47c471f0ebd6b57b1513f29a3b856
Alexander Zvenigorodsky <3175403+azvenigo@users.noreply.github.com>
2026-05-18 23:49:53 -0700

    Tweaking some control text for consistency with VS properties dialog

--- CommandLineToolWindow.cs
+++ CommandLineToolWindow.cs
@@ -23,7 +23,7 @@ namespace VS_LaunchArguments
         /// </summary>
         public CommandLineToolWindow() : base(null)
         {
-            this.Caption = "C++ CommandLine Args";
+            this.Caption = "Command Args";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
